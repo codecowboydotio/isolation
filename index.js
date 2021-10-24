3b7cbc29fd59d7343123c84dfbaa57d6fef2575f
codecowboydotio <scott@vankalken.org>
2021-10-24 11:56:47 +1100

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+const exec = require('child_process').exec;
+
+
+// View engine setup
+app.set('view engine', 'ejs');
+
+// Without middleware
+app.get('/', function(req, res){
+  goober = exec("ps -o uid -o pid -o ppid -o cmd --forest", (error, stdout, stderr) => {
+   if (error) {
+     console.error(`${error}`);
+     return;
+   }
+   fooble = (`${stdout}`);
+   newarray = fooble.split("\n");
+//   console.log(newarray[0]);
+  
+  // Rendering home.ejs page
+  res.render('home');
+  });
+});
+
+app.listen(PORT, function(err){
+	if (err) console.log(err);
+	console.log("Server listening on PORT", PORT);
+});
+
